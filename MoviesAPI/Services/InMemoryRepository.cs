362ca830f0f4574526251d99b1729775ae17df24
Gil <frogvsdogvsmarsmelo@gmail.com>
2023-03-01 17:46:13 +0200

    creating the repository

---
+++ MoviesAPI/Services/InMemoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using MoviesAPI.Entities;
+namespace MoviesAPI.Services
+{
+    public class InMemoryRepository : IRepository
+    {
+        private List<Genre> _genres;
+
+        public InMemoryRepository()
+        {
+            _genres = new List<Genre>()
+            {
+                new Genre(){id=1,Name="דרמה"},
+                new Genre(){id=2,Name="אקשן"}
+            };
+        }
+        public List<Genre> GetAllGenres()
+        {
+            return _genres;
+        }
+    }
+}
